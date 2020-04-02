649e47d9fcdf3d9803e22520d97662a36b7626e3
Štefan Hudáč <stefan.hudac@datax.biz>
2020-04-02 07:15:57 +0200

    Added brew installs file

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install MacOS Applications
+brew cask install sublime-text
+brew cask install google-chrome
+#brew cask install google-backup-and-sync
+brew cask install google-drive-file-stream
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install skype
+brew cask install vlc
+brew cask install docker
+brew cask install docker-toolbox
