47975d357946b1e48811e8531d9412f24f17cf79
Štefan Hudáč <stefan.hudac@datax.biz>
2020-04-02 07:55:13 +0200

    added more files

--- brew.sh
+++ brew.sh
@@ -3,11 +3,28 @@
 # Install MacOS Applications
 brew cask install sublime-text
 brew cask install google-chrome
-#brew cask install google-backup-and-sync
 brew cask install google-drive-file-stream
-brew cask install virtualbox
-brew cask install sourcetree
+#brew cask install google-backup-and-sync
+brew cask install the-unarchiver
+brew cask install disk-inventory-x
+brew cask install cyberduck
 brew cask install skype
+brew cask install virtualbox
+brew cask install balenaetcher
+
+# Photo and video
 brew cask install vlc
+brew cask install exifrenamer
+brew cask install obs
+
+# 3D printer
+brew cask install ultimaker-cura
+
+# DEV tools
+brew cask install sourcetree
 brew cask install docker
 brew cask install docker-toolbox
+brew cask install visual-studio-code
+brew cask install intellij-idea
+brew cask install mysqlworkbench
+brew install jhipster
